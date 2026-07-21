19147b877905829b23513438390e16c11ea4dcba
Oleg Knyazev <down.limit@gmail.com>
2026-07-22 00:54:59 +0300

    Define localized neighbor quest dialogues

--- src/dialogueConfig.js
+++ src/dialogueConfig.js
@@ -1,15 +1,44 @@
-const HOME_NPC_GREETING = deepFreeze({
-  id: "home-npc-greeting",
-  speakerKey: "dialogue:homeGreeting.speaker",
-  lines: [
-    { textKey: "dialogue:homeGreeting.lines.hello" },
-    { textKey: "dialogue:homeGreeting.lines.quietVillage" },
-    { textKey: "dialogue:homeGreeting.lines.goodbye" },
-  ],
-});
+import { NEIGHBOR_DIALOGUE_IDS } from "./neighborQuest.js";
+
+const line = (textKey, values) => deepFreeze(values ? { textKey, values } : { textKey });
+const dialogue = (id, speakerKey, lines) => deepFreeze({ id, speakerKey, lines });
 
 export const DIALOGUE_DEFINITIONS = deepFreeze({
-  [HOME_NPC_GREETING.id]: HOME_NPC_GREETING,
+  [NEIGHBOR_DIALOGUE_IDS.homeIntro]: dialogue(
+    NEIGHBOR_DIALOGUE_IDS.homeIntro,
+    "dialogue:neighbor.speakers.home",
+    [line("dialogue:neighbor.homeIntro.first"), line("dialogue:neighbor.homeIntro.second")],
+  ),
+  [NEIGHBOR_DIALOGUE_IDS.homeReminder]: dialogue(
+    NEIGHBOR_DIALOGUE_IDS.homeReminder,
+    "dialogue:neighbor.speakers.home",
+    [line("dialogue:neighbor.homeReminder.first")],
+  ),
+  [NEIGHBOR_DIALOGUE_IDS.homeCompletion]: dialogue(
+    NEIGHBOR_DIALOGUE_IDS.homeCompletion,
+    "dialogue:neighbor.speakers.home",
+    [line("dialogue:neighbor.homeCompletion.first"), line("dialogue:neighbor.homeCompletion.second")],
+  ),
+  [NEIGHBOR_DIALOGUE_IDS.homeAfter]: dialogue(
+    NEIGHBOR_DIALOGUE_IDS.homeAfter,
+    "dialogue:neighbor.speakers.home",
+    [line("dialogue:neighbor.homeAfter.first")],
+  ),
+  [NEIGHBOR_DIALOGUE_IDS.streetBefore]: dialogue(
+    NEIGHBOR_DIALOGUE_IDS.streetBefore,
+    "dialogue:neighbor.speakers.street",
+    [line("dialogue:neighbor.streetBefore.first")],
+  ),
+  [NEIGHBOR_DIALOGUE_IDS.streetResponse]: dialogue(
+    NEIGHBOR_DIALOGUE_IDS.streetResponse,
+    "dialogue:neighbor.speakers.street",
+    [line("dialogue:neighbor.streetResponse.first"), line("dialogue:neighbor.streetResponse.second")],
+  ),
+  [NEIGHBOR_DIALOGUE_IDS.streetAfter]: dialogue(
+    NEIGHBOR_DIALOGUE_IDS.streetAfter,
+    "dialogue:neighbor.speakers.street",
+    [line("dialogue:neighbor.streetAfter.first")],
+  ),
 });
 
 export function getDialogueDefinition(dialogueId) {
