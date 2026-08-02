12d20a741a8c61e672ea03f26591c456357bd33a
Oleg Knyazev <down.limit@gmail.com>
2026-08-02 18:15:58 +0300

    Task #068: add all T1 and T2 Atoll segments

--- src/world/wildAtollDomain.js
+++ src/world/wildAtollDomain.js
@@ -5,119 +5,112 @@ export const WILD_ATOLL_SEGMENTS = Object.freeze({
   starter: "starter",
   forestT1: "forest-t1",
   mineT1: "mine-t1",
+  forestGroveT2: "forest-grove-t2",
+  forestWetlandsT2: "forest-wetlands-t2",
+  mineCrystalT2: "mine-crystal-t2",
+  mineDepthsT2: "mine-depths-t2",
 });
 
-export const WILD_ATOLL_ARENAS = Object.freeze({
-  root: "starter-root",
-  meadow: "starter-meadow",
-  stones: "starter-stones",
-  pond: "starter-pond",
-  thicket: "starter-thicket",
-  roots: "starter-roots",
-  slope: "starter-slope",
-  edge: "starter-edge",
-});
-
-export const WILD_ATOLL_STARTER_LEVELS = Object.freeze([
-  Object.freeze([WILD_ATOLL_ARENAS.root]),
-  Object.freeze([WILD_ATOLL_ARENAS.meadow, WILD_ATOLL_ARENAS.stones]),
-  Object.freeze([WILD_ATOLL_ARENAS.pond, WILD_ATOLL_ARENAS.thicket]),
-  Object.freeze([WILD_ATOLL_ARENAS.roots, WILD_ATOLL_ARENAS.slope]),
-  Object.freeze([WILD_ATOLL_ARENAS.edge]),
+const STARTER_RESOURCE_PATTERN = Object.freeze([
+  Object.freeze(["log-small", "stone-small", "berry-bush"]),
+  Object.freeze(["log-small", "berry-bush", "berry-bush"]),
+  Object.freeze(["stone-small", "stone-small", "berry-bush"]),
+  Object.freeze(["log-small", "stone-small", "berry-bush", "berry-bush"]),
+  Object.freeze(["log-small", "log-small", "berry-bush"]),
+  Object.freeze(["log-small", "log-small", "stone-small"]),
+  Object.freeze(["stone-small", "stone-small", "log-small"]),
+  Object.freeze([]),
 ]);
 
-export const WILD_ATOLL_STARTER_ARENAS = Object.freeze(WILD_ATOLL_STARTER_LEVELS.flat());
+const FOREST_RESOURCE_PATTERN = Object.freeze([
+  Object.freeze(["log-small", "berry-bush", "berry-bush"]),
+  Object.freeze(["log-small", "log-small", "berry-bush"]),
+  Object.freeze(["log-small", "stone-small", "berry-bush"]),
+  Object.freeze(["log-small", "berry-bush", "berry-bush"]),
+  Object.freeze(["log-small", "log-small", "stone-small"]),
+  Object.freeze(["log-small", "berry-bush", "stone-small"]),
+  Object.freeze(["log-small", "log-small", "berry-bush"]),
+  Object.freeze([]),
+]);
 
-const ARENA_DEFINITIONS = Object.freeze({
-  [WILD_ATOLL_ARENAS.root]: arena({
-    id: WILD_ATOLL_ARENAS.root,
-    arenaKey: "hud:atoll.arenas.root",
-    resources: ["log-small", "stone-small", "berry-bush"],
-    exits: [
-      path("meadow", WILD_ATOLL_ARENAS.meadow, "north-west", "hud:atoll.paths.meadow"),
-      path("stones", WILD_ATOLL_ARENAS.stones, "north-east", "hud:atoll.paths.stones"),
-    ],
+const MINE_RESOURCE_PATTERN = Object.freeze([
+  Object.freeze(["stone-small", "stone-small", "berry-bush"]),
+  Object.freeze(["stone-small", "stone-small", "log-small"]),
+  Object.freeze(["stone-small", "berry-bush", "log-small"]),
+  Object.freeze(["stone-small", "stone-small", "berry-bush"]),
+  Object.freeze(["stone-small", "stone-small", "log-small"]),
+  Object.freeze(["stone-small", "berry-bush", "log-small"]),
+  Object.freeze(["stone-small", "stone-small", "stone-small"]),
+  Object.freeze([]),
+]);
+
+const SEGMENT_SPECS = Object.freeze([
+  segmentSpec({
+    id: WILD_ATOLL_SEGMENTS.starter,
+    segmentKey: "atoll:segments.starter",
+    resourcePattern: STARTER_RESOURCE_PATTERN,
+    nextSegments: [WILD_ATOLL_SEGMENTS.forestT1, WILD_ATOLL_SEGMENTS.mineT1],
   }),
-  [WILD_ATOLL_ARENAS.meadow]: arena({
-    id: WILD_ATOLL_ARENAS.meadow,
-    arenaKey: "hud:atoll.arenas.meadow",
-    resources: ["log-small", "berry-bush", "berry-bush"],
-    exits: [
-      path("pond", WILD_ATOLL_ARENAS.pond, "north-west", "hud:atoll.paths.pond"),
-      path("thicket", WILD_ATOLL_ARENAS.thicket, "north-east", "hud:atoll.paths.thicket"),
-    ],
+  segmentSpec({
+    id: WILD_ATOLL_SEGMENTS.forestT1,
+    segmentKey: "atoll:segments.forestT1",
+    resourcePattern: FOREST_RESOURCE_PATTERN,
+    nextSegments: [WILD_ATOLL_SEGMENTS.forestGroveT2, WILD_ATOLL_SEGMENTS.forestWetlandsT2],
   }),
-  [WILD_ATOLL_ARENAS.stones]: arena({
-    id: WILD_ATOLL_ARENAS.stones,
-    arenaKey: "hud:atoll.arenas.stones",
-    resources: ["stone-small", "stone-small", "berry-bush"],
-    exits: [
-      path("pond", WILD_ATOLL_ARENAS.pond, "north-west", "hud:atoll.paths.pond"),
-      path("thicket", WILD_ATOLL_ARENAS.thicket, "north-east", "hud:atoll.paths.thicket"),
-    ],
+  segmentSpec({
+    id: WILD_ATOLL_SEGMENTS.mineT1,
+    segmentKey: "atoll:segments.mineT1",
+    resourcePattern: MINE_RESOURCE_PATTERN,
+    nextSegments: [WILD_ATOLL_SEGMENTS.mineCrystalT2, WILD_ATOLL_SEGMENTS.mineDepthsT2],
   }),
-  [WILD_ATOLL_ARENAS.pond]: arena({
-    id: WILD_ATOLL_ARENAS.pond,
-    arenaKey: "hud:atoll.arenas.pond",
-    resources: ["log-small", "stone-small", "berry-bush", "berry-bush"],
-    exits: [
-      path("roots", WILD_ATOLL_ARENAS.roots, "north-west", "hud:atoll.paths.roots"),
-      path("slope", WILD_ATOLL_ARENAS.slope, "north-east", "hud:atoll.paths.slope"),
-    ],
+  segmentSpec({
+    id: WILD_ATOLL_SEGMENTS.forestGroveT2,
+    segmentKey: "atoll:segments.forestGroveT2",
+    resourcePattern: FOREST_RESOURCE_PATTERN,
   }),
-  [WILD_ATOLL_ARENAS.thicket]: arena({
-    id: WILD_ATOLL_ARENAS.thicket,
-    arenaKey: "hud:atoll.arenas.thicket",
-    resources: ["log-small", "log-small", "berry-bush"],
-    exits: [
-      path("roots", WILD_ATOLL_ARENAS.roots, "north-west", "hud:atoll.paths.roots"),
-      path("slope", WILD_ATOLL_ARENAS.slope, "north-east", "hud:atoll.paths.slope"),
-    ],
+  segmentSpec({
+    id: WILD_ATOLL_SEGMENTS.forestWetlandsT2,
+    segmentKey: "atoll:segments.forestWetlandsT2",
+    resourcePattern: FOREST_RESOURCE_PATTERN,
   }),
-  [WILD_ATOLL_ARENAS.roots]: arena({
-    id: WILD_ATOLL_ARENAS.roots,
-    arenaKey: "hud:atoll.arenas.roots",
-    resources: ["log-small", "log-small", "stone-small"],
-    exits: [path("edge", WILD_ATOLL_ARENAS.edge, "north", "hud:atoll.paths.edge")],
+  segmentSpec({
+    id: WILD_ATOLL_SEGMENTS.mineCrystalT2,
+    segmentKey: "atoll:segments.mineCrystalT2",
+    resourcePattern: MINE_RESOURCE_PATTERN,
   }),
-  [WILD_ATOLL_ARENAS.slope]: arena({
-    id: WILD_ATOLL_ARENAS.slope,
-    arenaKey: "hud:atoll.arenas.slope",
-    resources: ["stone-small", "stone-small", "log-small"],
-    exits: [path("edge", WILD_ATOLL_ARENAS.edge, "north", "hud:atoll.paths.edge")],
+  segmentSpec({
+    id: WILD_ATOLL_SEGMENTS.mineDepthsT2,
+    segmentKey: "atoll:segments.mineDepthsT2",
+    resourcePattern: MINE_RESOURCE_PATTERN,
   }),
-  [WILD_ATOLL_ARENAS.edge]: arena({
-    id: WILD_ATOLL_ARENAS.edge,
-    arenaKey: "hud:atoll.arenas.edge",
-    resources: [],
-    terminal: true,
-    exits: [
-      Object.freeze({
-        id: "forest-t1",
-        kind: "segment",
-        targetSegmentId: WILD_ATOLL_SEGMENTS.forestT1,
-        direction: "north-west",
-        promptKey: "hud:atoll.paths.forestT1",
-        blockedMessageKey: "hud:atoll.segmentLocked.forestT1",
-      }),
-      Object.freeze({
-        id: "mine-t1",
-        kind: "segment",
-        targetSegmentId: WILD_ATOLL_SEGMENTS.mineT1,
-        direction: "north-east",
-        promptKey: "hud:atoll.paths.mineT1",
-        blockedMessageKey: "hud:atoll.segmentLocked.mineT1",
-      }),
-      Object.freeze({
-        id: "nest",
-        kind: "teleport",
-        targetWorldId: WORLD_IDS.nest,
-        direction: "center",
-        promptKey: "hud:atoll.paths.nest",
-      }),
-    ],
-  }),
+]);
+
+const SEGMENT_DEFINITIONS = new Map();
+const ARENA_DEFINITIONS = new Map();
+for (const spec of SEGMENT_SPECS) {
+  const definition = buildSegment(spec);
+  SEGMENT_DEFINITIONS.set(definition.id, definition);
+  for (const arena of definition.arenas) ARENA_DEFINITIONS.set(arena.id, arena);
+}
+
+const starterDefinition = SEGMENT_DEFINITIONS.get(WILD_ATOLL_SEGMENTS.starter);
+export const WILD_ATOLL_ARENAS = Object.freeze({
+  root: starterDefinition.entryArenaId,
+  meadow: starterDefinition.levels[1][0],
+  stones: starterDefinition.levels[1][1],
+  pond: starterDefinition.levels[2][0],
+  thicket: starterDefinition.levels[2][1],
+  roots: starterDefinition.levels[3][0],
+  slope: starterDefinition.levels[3][1],
+  edge: starterDefinition.terminalArenaId,
 });
+
+export const WILD_ATOLL_STARTER_LEVELS = starterDefinition.levels;
+export const WILD_ATOLL_STARTER_ARENAS = starterDefinition.arenaIds;
+export const WILD_ATOLL_SEGMENT_IDS = Object.freeze(SEGMENT_SPECS.map(({ id }) => id));
+export const WILD_ATOLL_ALL_ARENAS = Object.freeze(
+  WILD_ATOLL_SEGMENT_IDS.flatMap((segmentId) => SEGMENT_DEFINITIONS.get(segmentId).arenaIds),
+);
 
 const RESOURCE_CELLS = Object.freeze([
   Object.freeze({ x: 10, y: 16 }),
@@ -129,20 +122,30 @@ const RESOURCE_CELLS = Object.freeze([
 ]);
 
 export const WILD_ATOLL_LOCALIZATION_KEYS = Object.freeze([
-  "hud:atoll.arrival",
-  "hud:atoll.leftRun",
-  "hud:atoll.promptEnter",
-  "hud:atoll.segmentStarter",
-  "hud:atoll.segmentLocked.forestT1",
-  "hud:atoll.segmentLocked.mineT1",
-  ...Object.values(ARENA_DEFINITIONS).flatMap((definition) => [
-    definition.arenaKey,
-    ...definition.exits.flatMap((exit) => [exit.promptKey, exit.blockedMessageKey].filter(Boolean)),
-  ]),
+  "atoll:arrival",
+  "atoll:leftRun",
+  "atoll:promptEnter",
+  "atoll:paths.nest",
+  ...WILD_ATOLL_SEGMENT_IDS.flatMap((segmentId) => {
+    const segment = SEGMENT_DEFINITIONS.get(segmentId);
+    return [
+      segment.segmentKey,
+      ...segment.arenas.flatMap((arena) => [
+        arena.arenaKey,
+        ...arena.exits.map((exit) => exit.promptKey),
+      ]),
+    ];
+  }),
 ]);
 
+export function getWildAtollSegmentDefinition(segmentId) {
+  const definition = SEGMENT_DEFINITIONS.get(segmentId);
+  if (!definition) throw new Error(`Unknown Wild Atoll segment: ${String(segmentId)}`);
+  return definition;
+}
+
 export function getWildAtollArenaDefinition(arenaId) {
-  const definition = ARENA_DEFINITIONS[arenaId];
+  const definition = ARENA_DEFINITIONS.get(arenaId);
   if (!definition) throw new Error(`Unknown Wild Atoll arena: ${String(arenaId)}`);
   return definition;
 }
@@ -178,20 +181,99 @@ export function hashUnit(text) {
   return (hash >>> 0) / 0x100000000;
 }
 
-function arena({ id, arenaKey, resources, exits, terminal = false }) {
+function segmentSpec({ id, segmentKey, resourcePattern, nextSegments = [] }) {
   return Object.freeze({
     id,
-    segmentId: WILD_ATOLL_SEGMENTS.starter,
-    segmentKey: "hud:atoll.segmentStarter",
-    arenaKey,
-    resources: Object.freeze([...resources]),
-    exits: Object.freeze([...exits]),
-    terminal: Boolean(terminal),
+    segmentKey,
+    resourcePattern,
+    nextSegments: Object.freeze([...nextSegments]),
   });
 }
 
-function path(id, targetArenaId, direction, promptKey) {
-  return Object.freeze({ id, kind: "path", targetArenaId, direction, promptKey });
+function buildSegment(spec) {
+  const ids = Object.freeze({
+    root: `${spec.id}-root`,
+    left1: `${spec.id}-left-1`,
+    right1: `${spec.id}-right-1`,
+    left2: `${spec.id}-left-2`,
+    right2: `${spec.id}-right-2`,
+    left3: `${spec.id}-left-3`,
+    right3: `${spec.id}-right-3`,
+    edge: `${spec.id}-edge`,
+  });
+  const levels = deepFreeze([
+    [ids.root],
+    [ids.left1, ids.right1],
+    [ids.left2, ids.right2],
+    [ids.left3, ids.right3],
+    [ids.edge],
+  ]);
+  const arenaIds = Object.freeze(levels.flat());
+  const resourcesById = new Map(arenaIds.map((arenaId, index) => [arenaId, spec.resourcePattern[index]]));
+  const arena = (arenaId, exits, terminal = false) => Object.freeze({
+    id: arenaId,
+    segmentId: spec.id,
+    segmentKey: spec.segmentKey,
+    arenaKey: `atoll:arenas.${spec.id}.${arenaNodeName(arenaId)}`,
+    resources: resourcesById.get(arenaId),
+    exits: Object.freeze(exits),
+    terminal,
+  });
+  const pathTo = (targetArenaId, direction) => Object.freeze({
+    id: targetArenaId,
+    kind: "path",
+    targetArenaId,
+    direction,
+    promptKey: `atoll:paths.${spec.id}.${arenaNodeName(targetArenaId)}`,
+  });
+  const terminalExits = [
+    ...spec.nextSegments.map((targetSegmentId, index) => Object.freeze({
+      id: targetSegmentId,
+      kind: "segment",
+      targetSegmentId,
+      direction: index === 0 ? "north-west" : "north-east",
+      promptKey: `atoll:segmentPaths.${targetSegmentId}`,
+    })),
+    Object.freeze({
+      id: "nest",
+      kind: "teleport",
+      targetWorldId: WORLD_IDS.nest,
+      direction: "center",
+      promptKey: "atoll:paths.nest",
+    }),
+  ];
+  const arenas = Object.freeze([
+    arena(ids.root, [pathTo(ids.left1, "north-west"), pathTo(ids.right1, "north-east")]),
+    arena(ids.left1, [pathTo(ids.left2, "north-west"), pathTo(ids.right2, "north-east")]),
+    arena(ids.right1, [pathTo(ids.left2, "north-west"), pathTo(ids.right2, "north-east")]),
+    arena(ids.left2, [pathTo(ids.left3, "north-west"), pathTo(ids.right3, "north-east")]),
+    arena(ids.right2, [pathTo(ids.left3, "north-west"), pathTo(ids.right3, "north-east")]),
+    arena(ids.left3, [pathTo(ids.edge, "north")]),
+    arena(ids.right3, [pathTo(ids.edge, "north")]),
+    arena(ids.edge, terminalExits, true),
+  ]);
+  return Object.freeze({
+    id: spec.id,
+    segmentKey: spec.segmentKey,
+    entryArenaId: ids.root,
+    terminalArenaId: ids.edge,
+    levels,
+    arenaIds,
+    arenas,
+    nextSegmentIds: spec.nextSegments,
+  });
+}
+
+function arenaNodeName(arenaId) {
+  if (arenaId.endsWith("-root")) return "root";
+  if (arenaId.endsWith("-left-1")) return "left1";
+  if (arenaId.endsWith("-right-1")) return "right1";
+  if (arenaId.endsWith("-left-2")) return "left2";
+  if (arenaId.endsWith("-right-2")) return "right2";
+  if (arenaId.endsWith("-left-3")) return "left3";
+  if (arenaId.endsWith("-right-3")) return "right3";
+  if (arenaId.endsWith("-edge")) return "edge";
+  throw new Error(`Unknown Wild Atoll arena node: ${arenaId}`);
 }
 
 function seededOrder(values, seed) {
@@ -199,4 +281,12 @@ function seededOrder(values, seed) {
     .map((value, index) => ({ value, rank: hashUnit(`${seed}:${index}`) }))
     .sort((left, right) => left.rank - right.rank)
     .map((entry) => entry.value);
+}
+
+function deepFreeze(value) {
+  if (value && typeof value === "object" && !Object.isFrozen(value)) {
+    Object.freeze(value);
+    for (const child of Object.values(value)) deepFreeze(child);
+  }
+  return value;
 }
