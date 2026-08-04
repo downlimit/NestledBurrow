80782d396b2f45519b041741f6c7325fbe419c26
Oleg Knyazev <down.limit@gmail.com>
2026-08-04 21:08:52 +0300

    Task #071: keep route resolution before gaze ranking

--- src/build/liveAssetGeometry.js
+++ src/build/liveAssetGeometry.js
@@ -70,8 +70,6 @@ export function hydrateFacilityRuntimeDefinition(definition) {
     presentationPose: null,
     requiresFacing: true,
     facingDotThreshold: 0,
-    targetingMode: "facing-first",
-    targetingGroup: PLACEABLE_TARGETING_GROUP,
   });
 }
 
@@ -134,6 +132,8 @@ export function livePlaceableInteraction(definition, geometry, { position = "int
     "aimPosition",
     "presentationPose",
     "wakePosition",
+    "targetingMode",
+    "targetingGroup",
   ]);
   return Object.freeze({
     ...canonical,
@@ -141,8 +141,6 @@ export function livePlaceableInteraction(definition, geometry, { position = "int
     aimPosition: geometry.interactionCenter,
     requiresFacing: true,
     facingDotThreshold: 0,
-    targetingMode: "facing-first",
-    targetingGroup: PLACEABLE_TARGETING_GROUP,
   });
 }
 
